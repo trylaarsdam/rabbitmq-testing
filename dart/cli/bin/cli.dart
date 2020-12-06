e0d66add425819c8dc0322d860817741c82dd581
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2020-12-05 19:03:49 -0600

    dart bind to exchange

--- dart/cli/bin/cli.dart
+++ dart/cli/bin/cli.dart
@@ -1,6 +1,32 @@
-import 'package:cli/cli.dart' as cli;
+import "dart:io";
 import 'package:dart_amqp/dart_amqp.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   //print('Hello world: ${cli.calculate()}!');
+  var connectionSettings = ConnectionSettings(
+      host: "trek.thewcl.com",
+      port: 5672,
+      virtualHost: "/",
+      authProvider: const PlainAuthenticator('testing', 'password'));
+  // ignore: omit_local_variable_types
+  Client client = Client(settings: connectionSettings);
+
+  ProcessSignal.sigint.watch().listen((_) async {
+    await client.close();
+    exit(0);
+  });
+
+  /*client
+      .channel()
+      .then((Channel channel) => channel.queue('hello'))
+      .then((Queue queue) => queue.consume())
+      .then((Consumer consumer) => consumer.listen((AmqpMessage message) {
+            print('Recieved string: ${message.payloadAsString}');
+          }));*/
+  var channel = await client.channel();
+  var exchange = await channel.exchange("logs", ExchangeType.FANOUT);
+  var consumer = await exchange.bindPrivateQueueConsumer(null);
+  consumer.listen((message) {
+    print("Message recieved from exchange: ${message.payloadAsString}");
+  });
 }
